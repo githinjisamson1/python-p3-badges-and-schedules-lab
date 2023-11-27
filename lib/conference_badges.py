c35b9062a65ed62eaf06ea6ca3b362a1656ce17d
Samson Githinji <githinjisamson148@gmail.com>
2023-11-27 23:51:38 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,18 +1,18 @@
 def badge_maker(name):
     return f"Hello, my name is {name}."
 
-# call badge_maker for each element
+# !call badge_maker for each element
 def batch_badge_creator(names):
     badgeMessagesLc = [badge_maker(item) for item in names ]
     
     return badgeMessagesLc
 
 def assign_rooms(names):  
-    # initialize roomAssignments  
+    # !initialize roomAssignments  
     roomAssignments = []
     
-    # for each item assign then append to roomAssignments
-    # room => currentindex + 1
+    # !for each item assign then append to roomAssignments
+    # !room => currentindex + 1
     for item in names:
         assignment = f"Hello, {item}! You'll be assigned to room {names.index(item) + 1}!"
         
@@ -40,4 +40,4 @@ def printer(names):
     
    
 
-printer(["Arel", "Carol"])
+# printer(["Arel", "Carol"])
